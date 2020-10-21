84e84a77fd9405d2b77af8af3c839bd419d7840c
elina-mns <ellie.mansurova@gmail.com>
2020-10-21 12:16:35 -0400

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Elina Mansurova on 2020-10-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
